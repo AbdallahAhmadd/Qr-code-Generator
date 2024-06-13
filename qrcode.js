5d4fe5c9b35ab7d067491a99a4d6665302d67127
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-06-13 21:15:59 +0300

    feat(Generate): add api call

---
+++ qrcode.js
@@ -0,0 +1,15 @@
+document.getElementById('getStartedBtn').addEventListener('click', function() {
+   document.getElementById('wireframe1').style.display = 'none';
+   document.getElementById('wireframe2').style.display = 'block';
+
+});
+
+document.getElementById('generateBtn').addEventListener('click', function() {
+    var input = document.getElementById('urlInput').value;
+
+    qrcode.src= 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data='+input;
+    document.getElementById('main-box_qrcode').style.display = 'flex';
+
+
+
+});
