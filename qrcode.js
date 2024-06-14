9a49571619644ef844e34e4c4b233b97949ac090
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-06-14 10:58:53 +0300

    feat(Generate): add working cross label

--- qrcode.js
+++ qrcode.js
@@ -6,10 +6,12 @@ document.getElementById('getStartedBtn').addEventListener('click', function() {
 
 document.getElementById('generateBtn').addEventListener('click', function() {
     var input = document.getElementById('urlInput').value;
-
     qrcode.src= 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data='+input;
     document.getElementById('main-box_qrcode').style.display = 'flex';
+});
 
-
-
+document.getElementById('cross-label').addEventListener('click', function() {
+    var urlInput = document.getElementById('urlInput')
+    urlInput.value = '';
+    urlInput.focus();
 });
